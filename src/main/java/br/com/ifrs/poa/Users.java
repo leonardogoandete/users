7ca1bf7ca76dd1454ce18518a0366fdfb214c482
leo <leo@hotmail.com>
2023-09-02 16:58:57 -0300

    Gerando token com porta https

---
+++ src/main/java/br/com/ifrs/poa/Users.java
@@ -0,0 +1,26 @@
+package br.com.ifrs.poa;
+
+import io.smallrye.jwt.build.Jwt;
+import jakarta.annotation.security.PermitAll;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.jwt.Claims;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Path("/users")
+public class Users {
+
+    @GET
+    @Path("/jwt")
+    @PermitAll
+    @Produces(MediaType.TEXT_PLAIN)
+    public String generate() {
+        return Jwt.issuer("http://localhost:8080")
+                .upn("leonardogoandete")
+                .groups(new HashSet<>(Arrays.asList("User", "Admin")))
+                .claim(Claims.full_name, "Leonardo Goandete")
+                .sign();
+    }
+}
